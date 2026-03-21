06bae5213f666d63f02d7f9677e71ac9be4f4859
AlanGr12 <alanfutbol58@gmail.com>
2026-03-21 12:08:41 -0300

    ej4 term

--- ej4.js
+++ ej4.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
+obtenerPais('Argentina')
+
+
 async function obtenerPais(nombrePais){
     const respuesta = await axios.get(`https://restcountries.com/v3.1/name/${nombrePais}`)
     const pais = respuesta.data[0]
+
+    console.log(`El nombre del pais es ${pais.name.common}, su capital es ${pais.capital[0]}, su region es ${pais.region}, la cantidad de poblacion es ${pais.population}`)
 }   
