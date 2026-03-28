042187e6dba6333ac7a2d1bed848ec571339a20d
AlanGr12 <alanfutbol58@gmail.com>
2026-03-28 12:11:20 -0300

    ej9

--- ej9.js
+++ ej9.js
@@ -1,1 +1,32 @@
 
+function validarPassword(password){
+
+    let tieneNum = false
+    let tieneMayus = false
+
+    if(password.length < 8){
+        return console.log("Password invalida")
+    }
+
+    for(let i = 0 ; i < password.length ; i++){
+        let letra = password[i]
+        if(!isNaN(letra)){
+            tieneNum = true
+        }
+        else if(letra.toUpperCase() === password[i]){
+            tieneMayus = true
+        }
+
+    
+    }
+    
+    
+
+    if(tieneNum == true && tieneMayus == true ){
+        return console.log("Password valida")
+    }
+    else
+        return console.log("La password no contiene una mayuscula o un numero")
+    
+
+}
