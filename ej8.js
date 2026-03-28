8f92d2cc07237397a951c7865550930f0ae2d5ba
AlanGr12 <alanfutbol58@gmail.com>
2026-03-28 12:19:12 -0300

    tp terminado

--- ej8.js
+++ ej8.js
@@ -2,17 +2,17 @@ function analizarTexto(texto){
     let caracteres = texto.length
     let palabras = texto.split(" ").length
     let caracteresVocales = 0;
-    let cartacteresConso = 0;
+    let cartacteresConsonantes = 0;
 
     for(let i = 0; i < texto.length; i++){
         if('a' === texto[i] || 'e' === texto[i] || 'i' === texto[i] || 'o' === texto[i] || 'u' === texto[i]){
             caracteresVocales++;
         }
         else if('b' === texto[i] || 'c' === texto[i] || 'd' === texto[i] || 'f'=== texto[i] || 'g'=== texto[i] || 'h'=== texto[i] || 'j' === texto[i] || 'k' === texto[i] || 'l' === texto[i] || 'm' === texto[i] || 'p' === texto[i] || 'q' === texto[i] || 'r' === texto[i] || 's' === texto[i] || 't' === texto[i] || 'w' === texto[i] || 'x' === texto[i] || 'y' === texto[i] || 'z' === texto[i] )
-        cartacteresConso++
+        cartacteresConsonantes++
     }
 
-    let cantTexto = {caracteres,palabras,caracteresVocales,cartacteresConso}
+    let cantTexto = {caracteres,palabras,caracteresVocales,cartacteresConsonantes}
 
     return cantTexto
 }
